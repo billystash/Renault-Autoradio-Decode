879cb14e4c1712af98adf1839161725cad0d2043
Billy <nono_b56@hotmail.fr>
2025-06-11 21:58:07 +0200

    Change version to 1.0.1

--- src/main/java/com/billystash/renaultautoradiodecode/C/C.java
+++ src/main/java/com/billystash/renaultautoradiodecode/C/C.java
@@ -1,8 +1,8 @@
 package com.billystash.renaultautoradiodecode.C;
 
 public class C {
-    public static final String VERSION = "V1.0";
-    public static final String DATE = "22/05/2023";
+    public static final String VERSION = "V1.0.1";
+    public static final String DATE = "11/06/2025";
 
     public static final String APP_DATA_PATH = System.getProperty("user.home") + "\\AppData\\Local";
     public static final String APP_RAD_PATH = APP_DATA_PATH + "\\RAD\\";
